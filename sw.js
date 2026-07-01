583baa98682712fdc2c776b5237bc13b128060ba
Arnjnnngs <arnjnnngs@gmail.com>
2026-07-01 01:20:11 -0500

    Rename title to Brandi's Meds / Vitals Historical + SW cache v7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'caretracker-v6';
+const CACHE = 'caretracker-v7';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
