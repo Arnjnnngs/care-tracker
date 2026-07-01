6a750dd003d8f284c2391d91396fbdcd2600fa82
Arnjnnngs <arnjnnngs@gmail.com>
2026-06-30 23:40:05 -0500

    Glassmorphism UI + SW cache v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'caretracker-v2';
+const CACHE = 'caretracker-v3';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
