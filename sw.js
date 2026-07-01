cc47438ee99ed1db77df4d48fc48df72e71b135f
Arnjnnngs <arnjnnngs@gmail.com>
2026-07-01 09:09:02 -0500

    Defer re-renders while time modal is open + SW v10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'caretracker-v9';
+const CACHE = 'caretracker-v10';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
