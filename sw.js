84496d7cf6cdb5100f3603d3139566a6791877be
Arnjnnngs <arnjnnngs@gmail.com>
2026-07-01 21:59:36 -0500

    v14: Fix input focus loss on mobile during render cycle

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'caretracker-v13';
+const CACHE = 'caretracker-v14';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
