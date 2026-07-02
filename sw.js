27852a4e23379f89e2725e0c6cba37dd7bf086fe
Arnjnnngs <arnjnnngs@gmail.com>
2026-07-01 19:55:22 -0500

    v13: Bump SW cache to force refresh on all devices

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'caretracker-v12';
+const CACHE = 'caretracker-v13';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
