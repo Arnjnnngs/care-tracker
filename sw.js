591a271c77dd57290fc5586f6a1c654d0a2fb246
Arnjnnngs <arnjnnngs@gmail.com>
2026-07-07 22:36:48 -0500

    v19: Remove Clear all buttons, preserve history

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'caretracker-v18';
+const CACHE = 'caretracker-v19';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
