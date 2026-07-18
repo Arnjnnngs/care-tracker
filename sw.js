948e5a4bddfba4ad1e7e6e42108a2f634b75339c
Arnjnnngs <arnjnnngs@gmail.com>
2026-07-18 18:54:51 -0500

    Fix Dex Zofran chemo window logic v34

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'caretracker-v33';
+const CACHE = 'caretracker-v34';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
