8baa097083f57fbd0d431bf279d06ac289a5703e
Arnjnnngs <arnjnnngs@gmail.com>
2026-07-19 00:37:19 -0500

    v35 promote redesigned UI plus features from testing

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'caretracker-v34';
+const CACHE = 'caretracker-v35';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
